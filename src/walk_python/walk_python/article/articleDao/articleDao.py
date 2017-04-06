aa8ae9d2d3a7d0187558d64190a58ec8986958a9
lu.wei <lu.wei@jrj.com.cn>
2017-04-06 21:53:27 +0800

    add model

---
+++ src/walk_python/walk_python/article/articleDao/articleDao.py
@@ -0,0 +1,10 @@
+#-*- coding : UTF-8 -*-
+'''
+Created on 2017年4月6日
+
+@author: Administrator
+'''
+
+def saveArticle(articleParam):
+    pass
+    
--- src/walk_python/walk_python/article/articleService/service.py
+++ src/walk_python/walk_python/article/articleService/service.py
@@ -4,9 +4,11 @@ Created on 2017年4月3日
 
 @author: 01158130
 '''
+from walk_python.article.articleDao import articleDao
 
 def saveArticle(articleParam):
     if articleParam and articleParam.is_init:
+        articleDao.saveArticle(articleParam)
         
     
         
