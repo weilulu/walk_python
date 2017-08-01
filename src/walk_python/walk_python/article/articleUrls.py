0c3631afe6d4cad644084453b086a83fcfc5c297
W.lu <762990029@qq.com>
2017-08-01 17:52:33 +0800

    linux delopy

--- src/walk_python/walk_python/article/articleUrls.py
+++ src/walk_python/walk_python/article/articleUrls.py
@@ -5,7 +5,7 @@ Created on 2017年4月2日
 @author: W.lu
 '''
 
-from django.conf.urls import patterns
+from django.conf.urls import patterns, url, include
 from walk_python.article.articleAction import WriteArticle
 
 urlpatterns = patterns('',
